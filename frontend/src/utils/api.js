828edd0b3a645faf6d364dc926cf0d03fcea934c
bashairfan0911 <bashairfan0911@gmail.com>
2025-10-30 13:31:08 +0530

    Configure API to use environment variable

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-const baseURL = 'http://localhost:5000/';
-// const baseURL = 'https://eko-mart.onrender.com/'; // Use this for production
+const baseURL = import.meta.env.VITE_API_URL || 'http://localhost:5000/';
 
 const api = axios.create({
     baseURL : baseURL
