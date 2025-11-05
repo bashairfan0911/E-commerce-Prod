944969cd90d13247e51c1d20e7b6cd0cc09f03d7
bashairfan0911 <bashairfan0911@gmail.com>
2025-11-05 18:19:13 +0530

    fix: remove extensionsToTreatAsEsm from Jest config

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,10 +1,6 @@
 export default {
   testEnvironment: 'node',
   transform: {},
-  extensionsToTreatAsEsm: ['.js'],
-  moduleNameMapper: {
-    '^(\\.{1,2}/.*)\\.js$': '$1',
-  },
   coverageDirectory: 'coverage',
   collectCoverageFrom: [
     'Controller/**/*.js',
